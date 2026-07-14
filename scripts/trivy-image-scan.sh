835b2ac2ab05958364ae5cab354f29515ef1d524
nagarajucsd <dramarajudramaraju59@gmail.com>
2026-07-14 16:02:00 +0000

    Updated the trivy cache directory

--- scripts/trivy-image-scan.sh
+++ scripts/trivy-image-scan.sh
@@ -5,17 +5,18 @@ set -e
 IMAGE_NAME=${IMAGE_NAME:-production-devops-pipeline}
 IMAGE_TAG=${IMAGE_TAG:-latest}
 
+mkdir -p reports
+
 echo "====================================="
 echo "| Running Trivy Docker Image Scan   |"
 echo "====================================="
 
 echo "Scanning image: ${IMAGE_NAME}:${IMAGE_TAG}"
 
-mkdir -p reports
-
 trivy image \
-  --cache-dir .trivycache \
-  --timeout 15m \
+  --cache-dir ~/.cache/trivy \
+  --timeout 45m \
+  --skip-version-check \
   --scanners vuln \
   --format table \
   --output reports/trivy-image-report.txt \

971da2c6517b88d68d5a880b949d6871c6d73318
nagarajucsd <dramarajudramaraju59@gmail.com>
2026-07-18 04:21:55 +0000

    Updated the required code for health-check issues after inetgration of docker compose

--- scripts/docker-compose-deploy.sh
+++ scripts/docker-compose-deploy.sh
@@ -3,9 +3,13 @@
 set -e
 
 echo "Stopping existing Compose stack..."
-docker compose -f docker-compose/docker-compose.yml down
+docker compose \
+  --env-file docker-compose/.env \
+  -f docker-compose/docker-compose.yml down
 
 echo "Building and starting services..."
-docker compose -f docker-compose/docker-compose.yml up -d
+docker compose \
+  --env-file docker-compose/.env \
+  -f docker-compose/docker-compose.yml up -d
 
 echo "Deployment completed."
